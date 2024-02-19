ca39466a9958f81301b83a24a7a5918c3c40d58d
Ron Lee <rongtanglee@gmail.com>
2024-02-19 15:41:09 +0000

    Add replay-hid pipeline
    Signed-off-by: Ron Lee <rongtanglee@gmail.com>

---
+++ replay-hid.groovy
@@ -0,0 +1,64 @@
+def product_name
+def abort_recording = false
+
+pipeline {
+    agent {
+        label 'jenkins-master'
+    }
+    
+    parameters {
+        string defaultValue: '192.168.1.102', description: 'DUT IP address', name: 'dut_ip'
+        //base64File description: 'HID event file to be replayed', name: 'hid_file'
+        stashedFile description: 'Upload HID Event File', name: 'hid_file'
+        string defaultValue: '1', description: 'Replay Iteration', name: 'replay_iter'
+        string defaultValue: '3', description: 'Delay between Iterations', name: 'delay'
+    }
+    
+    stages {
+        stage('Check DUT') {
+            steps {
+                sh 'printenv'
+                echo "Check DUT IP: ${params.dut_ip}"
+                sh "ssh root@${params.dut_ip} 'crossystem'"
+                script {
+                    product_name = sh(script: "ssh root@${params.dut_ip} 'dmidecode -s baseboard-product-name'", returnStdout: true).trim()
+                    echo "product_name: ${product_name}"
+                }
+            }
+        }
+        
+        stage('Push tools to DUT') {
+            steps {
+                
+                dir("${env.HOME}/tools") {
+                    echo 'Push record-replay-hid tools to DUT'
+                    sh "scp record-replay-hid.zip root@${params.dut_ip}:/usr/local/"
+                    sh "ssh root@${params.dut_ip} 'cd /usr/local && unzip -o record-replay-hid.zip'"
+                }
+            }
+        }
+        
+        stage('Push HID file') {
+            steps {
+                withFileParameter('hid_file') {
+                    unstash 'hid_file'
+                    sh "scp hid_file root@${params.dut_ip}:/usr/local/record-replay-hid/replay.hid"
+                }
+            }
+        }
+        
+        stage('Replay HID file') {
+            steps {
+                echo "Replay HID file: replay.hid"
+                sh "ssh root@${params.dut_ip} 'cd /usr/local/record-replay-hid/ && ./replay-hid.py replay.hid ${params.replay_iter} ${params.delay}'"
+            }
+            
+            post {
+                aborted {
+                    echo "Stopping the replay-hid"
+                    sh "ssh root@${params.dut_ip} 'killall hid-replay'"
+                }
+            }
+        }
+    }
+}
